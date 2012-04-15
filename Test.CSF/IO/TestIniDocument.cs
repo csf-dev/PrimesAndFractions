d95b7d040e59117149f72507c9e7392c7bcc3e69
Craig Fowler <craig@craigfowler.me.uk>
2012-04-15 19:40:04 +0100

    Further tests for the IniDocument type

--- Test.CSF/IO/TestIniDocument.cs
+++ Test.CSF/IO/TestIniDocument.cs
@@ -45,6 +45,46 @@ value = another value");
       Assert.AreEqual("baz", document["bork"], "Correct value for 'bork'");
       Assert.AreEqual("someValue", document.Sections["test"]["sample"], "Correct value for 'test' » 'sample'");
     }
+    
+    [Test]
+    public void TestReadCommentedFile()
+    {
+      IIniDocument document = IniDocument.Read(@"foo = bar
+
+; This is a comment
+# This is a different comment
+
+bork = ""This is a quoted value""
+[test]
+sample = someValue
+[another test]
+value = another value");
+      
+      Assert.AreEqual(2, document.Sections.Count, "Correct count of sections");
+      Assert.AreEqual(2, document.Count, "Correct count of values");
+      Assert.AreEqual("\"This is a quoted value\"", document["bork"], "Correct value for 'bork'");
+      Assert.AreEqual("someValue", document.Sections["test"]["sample"], "Correct value for 'test' » 'sample'");
+    }
+    
+    [Test]
+    [ExpectedException(ExceptionType = typeof(FormatException))]
+    public void TestReadInvalidFile()
+    {
+      IniDocument.Read(@"foo = bar
+
+; This is a comment
+# This is a different comment
+
+this is an invalid line
+
+bork = ""This is a quoted value""
+[test]
+sample = someValue
+[another test]
+value = another value");
+      
+      Assert.Fail("Test should not reach this point");
+    }
   }
 }
 
