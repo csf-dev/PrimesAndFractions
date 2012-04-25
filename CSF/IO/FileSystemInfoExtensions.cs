c913b37872c070ad5534b74394944d81280818e2
Craig Fowler <craig@craigfowler.me.uk>
2012-04-25 20:23:43 +0100

    Added FileSystemInfoExtensions and tests

---
+++ CSF/IO/FileSystemInfoExtensions.cs
@@ -0,0 +1,108 @@
+//  
+//  PathInfoExtensions.cs
+//  
+//  Author:
+//       Craig Fowler <craig@craigfowler.me.uk>
+// 
+//  Copyright (c) 2012 Craig Fowler
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.IO;
+
+namespace CSF.IO
+{
+  /// <summary>
+  /// Extension methods for <see cref="FileSystemInfo"/>.
+  /// </summary>
+  public static class FileSystemInfoExtensions
+  {
+    /// <summary>
+    /// Determines whether this instance is child of the specified directory.
+    /// </summary>
+    /// <returns>
+    /// <c>true</c> if this instance is child of the specified directory; otherwise, <c>false</c>.
+    /// </returns>
+    /// <param name='info'>
+    /// The current <see cref="FileSystemInfo"/> instance.
+    /// </param>
+    /// <param name='directory'>
+    /// The directory to test against.
+    /// </param>
+    public static bool IsChildOf(this FileSystemInfo info, DirectoryInfo directory)
+    {
+      return info.FullName.StartsWith(directory.FullName);
+    }
+    
+    /// <summary>
+    /// Gets a relative path that represents the current instance's relative path from a given <paramref name="root"/>.
+    /// </summary>
+    /// <returns>
+    /// The relative path component.
+    /// </returns>
+    /// <param name='info'>
+    /// The current <see cref="FileSystemInfo"/> instance.
+    /// </param>
+    /// <param name='root'>
+    /// The root directory from which to create the output
+    /// </param>
+    /// <exception cref='ArgumentException'>
+    /// Is thrown if the current instance is not a child of the root directory.
+    /// </exception>
+    public static string GetRelative(this FileSystemInfo info, DirectoryInfo root)
+    {
+      if(!info.IsChildOf(root))
+      {
+        throw new ArgumentException("Parameter 'info' is not a child of the root directory.");
+      }
+      
+      return info.FullName.Substring(root.FullName.Length);
+    }
+    
+    /// <summary>
+    /// Gets the parent of the current instance, or null if the current instance is the root of its filesystem.
+    /// </summary>
+    /// <returns>
+    /// The parent directory.
+    /// </returns>
+    /// <param name='info'>
+    /// The current <see cref="FileSystemInfo"/> instance.
+    /// </param>
+    /// <exception cref='ArgumentNullException'>
+    /// Is thrown when an argument passed to a method is invalid because it is <see langword="null" /> .
+    /// </exception>
+    public static DirectoryInfo GetParent(this FileSystemInfo info)
+    {
+      DirectoryInfo output;
+      DirectoryInfo directory = info as DirectoryInfo;
+      FileInfo file = info as FileInfo;
+      
+      if(directory != null)
+      {
+        output = directory.Parent;
+      }
+      else if(file != null)
+      {
+        output = file.Directory;
+      }
+      else
+      {
+        throw new ArgumentNullException("info");
+      }
+      
+      return output;
+    }
+  }
+}
+
---
+++ Test.CSF/IO/TestFileSystemInfoExtensions.cs
@@ -0,0 +1,59 @@
+using System;
+using NUnit.Framework;
+using CSF.IO;
+using System.IO;
+
+namespace Test.CSF.IO
+{
+  [TestFixture]
+  public class TestFileSystemInfoExtensions
+  {
+    #region tests
+    
+    [Test]
+    public void TestIsChildOf()
+    {
+      FileInfo file = new FileInfo(@"C:\SomeDirectory\SomeFile.txt");
+      
+      Assert.IsTrue(file.IsChildOf(new DirectoryInfo(@"C:\SomeDirectory")), "Is child of C:\\SomeDirectory");
+      Assert.IsFalse(file.IsChildOf(new DirectoryInfo(@"C:\OtherDirectory")), "Is not child of C:\\OtherDirectory");
+    }
+    
+    [Test]
+    public void TestGetRelative()
+    {
+      FileInfo file = new FileInfo(@"C:\SomeDirectory\SomeFile.txt");
+      
+      Assert.AreEqual(@"SomeDirectory\SomeFile.txt",
+                      file.GetRelative(new DirectoryInfo(@"C:\")),
+                      "Correct relative path");
+    }
+    
+    [Test]
+    [ExpectedException(ExceptionType = typeof(ArgumentException))]
+    public void TestGetRelativeNotRooted()
+    {
+      FileInfo file = new FileInfo(@"C:\SomeDirectory\SomeFile.txt");
+      file.GetRelative(new DirectoryInfo(@"D:\"));
+      Assert.Fail("Test should not reach this point");
+    }
+    
+    [Test]
+    public void TestGetParent()
+    {
+      DirectoryInfo currentDir = new DirectoryInfo(System.Environment.CurrentDirectory);
+      FileInfo file = new FileInfo(currentDir.FullName + Path.DirectorySeparatorChar + "testFile.txt");
+      
+      Assert.AreEqual(currentDir,
+                      file.GetParent(),
+                      "File");
+      Assert.AreEqual(currentDir.Parent,
+                      currentDir.GetParent(),
+                      "Directory");
+      Assert.IsNull(currentDir.Root.GetParent(), "Filesystem root");
+    }
+    
+    #endregion
+  }
+}
+
