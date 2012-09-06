c8a3b45b7df0102fe23bfccdb62e7aa733b10eff
Craig Fowler <craig@craigfowler.me.uk>
2012-09-06 16:24:43 +0100

    Added new extension methods to 'Type' that handle checking for interfaces
    These are really convenience methods.  Included tests for these also.

--- CSF/Reflection/TypeExtensions.cs
+++ CSF/Reflection/TypeExtensions.cs
@@ -22,6 +22,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace CSF.Reflection
 {
@@ -30,6 +31,12 @@ namespace CSF.Reflection
   /// </summary>
   public static class TypeExtensions
   {
+    #region constants
+
+    private static readonly Regex NameMangler = new Regex(@"(`\d+)\[.+\]$", RegexOptions.Compiled);
+
+    #endregion
+
     #region extension methods
     
     /// <summary>
@@ -90,6 +97,179 @@ namespace CSF.Reflection
       }
       
       return searchAssembly.GetTypes().Where(x => x.IsSubclassOf(type)).ToList();
+    }
+
+    /// <summary>
+    /// Gets an interface potentially implemented by the current type.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Beware when using this method with generic interfaces.  Due to FCL framework limitations, this method will find
+    /// the interface as long as the type implements an interface of the same name with the same number of generic
+    /// parameters.  It does NOT check the types of those generic parameters in the case of a 'constructed' generic
+    /// interface.
+    /// </para>
+    /// <example>
+    /// For example, the following could occur:
+    /// <code>
+    /// interface GenericInterface&lt;T&gt; {}
+    /// class MyClass : GenericInterface&lt;int&gt; {}
+    /// 
+    /// typeof(MyClass).GetInterface&lt;GenericInterface&lt;string&gt;&gt;() // Does not return null!
+    /// typeof(MyClass).ImplementsInterface&lt;GenericInterface&lt;string&gt;&gt;() // Returns true!
+    /// </code>
+    /// </example>
+    /// </remarks>
+    /// <returns>
+    /// The interface type, or a null reference if the type does not implement the desired interface.
+    /// </returns>
+    /// <param name='type'>
+    /// The type upon which to search for the interface
+    /// </param>
+    /// <typeparam name='TInterface'>
+    /// The type of the interface to search for.
+    /// </typeparam>
+    public static Type GetInterface<TInterface>(this Type type)
+    {
+      return type.GetInterface(typeof(TInterface));
+    }
+
+    /// <summary>
+    /// Gets an interface potentially implemented by the current type.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Beware when using this method with generic interfaces.  Due to FCL framework limitations, this method will find
+    /// the interface as long as the type implements an interface of the same name with the same number of generic
+    /// parameters.  It does NOT check the types of those generic parameters in the case of a 'constructed' generic
+    /// interface.
+    /// </para>
+    /// <example>
+    /// For example, the following could occur:
+    /// <code>
+    /// interface GenericInterface&lt;T&gt; {}
+    /// class MyClass : GenericInterface&lt;int&gt; {}
+    /// 
+    /// typeof(MyClass).GetInterface&lt;GenericInterface&lt;string&gt;&gt;() // Does not return null!
+    /// typeof(MyClass).ImplementsInterface&lt;GenericInterface&lt;string&gt;&gt;() // Returns true!
+    /// </code>
+    /// </example>
+    /// </remarks>
+    /// <returns>
+    /// The interface type, or a null reference if the type does not implement the desired interface.
+    /// </returns>
+    /// <param name='type'>
+    /// The type upon which to search for the interface
+    /// </param>
+    /// <param name='interfaceType'>
+    /// The type of the interface to search for.
+    /// </param>
+    public static Type GetInterface(this Type type, Type interfaceType)
+    {
+      if(interfaceType == null)
+      {
+        throw new ArgumentNullException("interfaceType");
+      }
+      else if(!interfaceType.IsInterface)
+      {
+        throw new ArgumentException("Interface type is not an interface!");
+      }
+
+      // FIXME: There is a bug described in the remarks to this method, it should be fixed!
+
+      return type.GetInterface(interfaceType.GetMangledName(), false);
+    }
+
+    /// <summary>
+    /// Determines whether the current type implements the specified interface.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Beware when using this method with generic interfaces.  Due to FCL framework limitations, this method will find
+    /// the interface as long as the type implements an interface of the same name with the same number of generic
+    /// parameters.  It does NOT check the types of those generic parameters in the case of a 'constructed' generic
+    /// interface.
+    /// </para>
+    /// <example>
+    /// For example, the following could occur:
+    /// <code>
+    /// interface GenericInterface&lt;T&gt; {}
+    /// class MyClass : GenericInterface&lt;int&gt; {}
+    /// 
+    /// typeof(MyClass).GetInterface&lt;GenericInterface&lt;string&gt;&gt;() // Does not return null!
+    /// typeof(MyClass).ImplementsInterface&lt;GenericInterface&lt;string&gt;&gt;() // Returns true!
+    /// </code>
+    /// </example>
+    /// </remarks>
+    /// <returns>
+    /// True if the <paramref name="type"/> implements the desired interface, false otherwise.
+    /// </returns>
+    /// <param name='type'>
+    /// The type upon which to search for the interface
+    /// </param>
+    /// <typeparam name='TInterface'>
+    /// The type of the interface to search for.
+    /// </typeparam>
+    public static bool ImplementsInterface<TInterface>(this Type type)
+    {
+      return (type.GetInterface(typeof(TInterface)) != null);
+    }
+
+    /// <summary>
+    /// Determines whether the current type implements the specified interface.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Beware when using this method with generic interfaces.  Due to FCL framework limitations, this method will find
+    /// the interface as long as the type implements an interface of the same name with the same number of generic
+    /// parameters.  It does NOT check the types of those generic parameters in the case of a 'constructed' generic
+    /// interface.
+    /// </para>
+    /// <example>
+    /// For example, the following could occur:
+    /// <code>
+    /// interface GenericInterface&lt;T&gt; {}
+    /// class MyClass : GenericInterface&lt;int&gt; {}
+    /// 
+    /// typeof(MyClass).GetInterface&lt;GenericInterface&lt;string&gt;&gt;() // Does not return null!
+    /// typeof(MyClass).ImplementsInterface&lt;GenericInterface&lt;string&gt;&gt;() // Returns true!
+    /// </code>
+    /// </example>
+    /// </remarks>
+    /// <returns>
+    /// True if the <paramref name="type"/> implements the desired interface, false otherwise.
+    /// </returns>
+    /// <param name='type'>
+    /// The type upon which to search for the interface
+    /// </param>
+    /// <param name='interfaceType'>
+    /// The type of the interface to search for.
+    /// </param>
+    public static bool ImplementsInterface(this Type type, Type interfaceType)
+    {
+      return (type.GetInterface(interfaceType) != null);
+    }
+
+    /// <summary>
+    /// Gets the 'mangled' name of a type (removing any generic parameter types, leaving only the number of generic
+    /// parameters).
+    /// </summary>
+    /// <returns>
+    /// The mangled name.
+    /// </returns>
+    /// <param name='type'>
+    /// The type for which to get the mangled name.
+    /// </param>
+    public static string GetMangledName(this Type type)
+    {
+      string output = type.FullName;
+
+      if(NameMangler.IsMatch(output))
+      {
+        output = NameMangler.Replace(output, @"$1");
+      }
+
+      return output;
     }
 
     #endregion
--- Test.CSF/Reflection/TestTypeExtensions.cs
+++ Test.CSF/Reflection/TestTypeExtensions.cs
@@ -20,6 +20,53 @@ namespace Test.CSF.Reflection
       Assert.IsTrue(types.Contains(typeof(Bar)), "Contains 'bar'");
       Assert.IsTrue(types.Contains(typeof(Baz)), "Contains 'baz'");
     }
+
+    [Test]
+    public void TestGetInterface()
+    {
+      Type testType = typeof(Bar);
+
+      Assert.AreEqual(typeof(IMarker), testType.GetInterface<IMarker>(), "Implements IMarker");
+      Assert.IsNull(testType.GetInterface<IDisposable>(), "Does not implement IDisposable");
+    }
+
+    [Test]
+    public void TestImplementsInterface()
+    {
+      Type testType = typeof(Bar);
+
+      Assert.IsTrue(testType.ImplementsInterface<IMarker>(), "Implements IMarker");
+      Assert.IsFalse(testType.ImplementsInterface<IDisposable>(), "Does not implement IDisposable");
+    }
+
+    [Test]
+    public void TestGetInterfaceGeneric()
+    {
+      Type testType = typeof(Baz);
+
+      Assert.AreEqual(typeof(IMarker<int>), testType.GetInterface<IMarker<int>>(), "Implements IMarker<int>");
+      Assert.IsNull(testType.GetInterface<IDisposable>(), "Does not implement IDisposable");
+    }
+
+    [Test]
+    public void TestImplementsInterfaceGeneric()
+    {
+      Type testType = typeof(Baz);
+
+      Assert.IsTrue(testType.ImplementsInterface<IMarker<int>>(), "Implements IMarker<int>");
+      Assert.IsFalse(testType.ImplementsInterface<IDisposable>(), "Does not implement IDisposable");
+    }
+
+    [Test]
+    public void TestGetMangledName()
+    {
+      Assert.AreEqual("Test.CSF.Reflection.TestTypeExtensions+Baz",
+                      typeof(Baz).GetMangledName(),
+                      "Correct for non-generic type");
+      Assert.AreEqual("Test.CSF.Reflection.TestTypeExtensions+IMarker`1",
+                      typeof(IMarker<int>).GetMangledName(),
+                      "Correct for generic type");
+    }
     
     #endregion
     
@@ -27,9 +74,13 @@ namespace Test.CSF.Reflection
     
     class Foo {}
     
-    class Bar : Foo {}
+    class Bar : Foo, IMarker {}
     
-    class Baz : Bar {}
+    class Baz : Bar, IMarker<int> {}
+
+    interface IMarker {}
+
+    interface IMarker<T> {}
     
     #endregion
   }
