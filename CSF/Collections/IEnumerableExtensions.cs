4aaca8419c13be46b73832319a6a224addc15684
Craig Fowler <craig@craigfowler.me.uk>
2013-03-20 21:52:56 +0000

    Resolve #15 - alter namespace of IEnumerableExtensions to CSF.Collections
    This functionality shouldn't have been in the root namespace in the first place.
    
    * Functionality and tests still exist for the old namespace but are marked
      obsolete.  In the next major version they may be removed.
    
    * Tests added for functionality in new namespace.

--- CSF/Collections/IEnumerableExtensions.cs
+++ CSF/Collections/IEnumerableExtensions.cs
@@ -21,9 +21,8 @@
 using System;
 using System.Collections;
 using System.Text;
-using CSF.Collections;
 
-namespace CSF
+namespace CSF.Collections
 {
   /// <summary>
   /// Static helper class containing extension methods for <see cref="IEnumerable"/> types.
@@ -45,10 +44,51 @@ namespace CSF
     /// <exception cref='ArgumentNullException'>
     /// Is thrown when an argument passed to a method is invalid because it is <see langword="null" /> .
     /// </exception>
-    [Obsolete("This type and its associated extension methods are being moved to the 'CSF.Collections' namespace.")]
     public static string ToSeparatedString(this IEnumerable collection, string separator)
     {
-      return CSF.Collections.IEnumerableExtensions.CreateSeparatedString(collection, separator);
+      return CreateSeparatedString(collection, separator);
+    }
+
+    /// <summary>
+    /// Returns the collection of items as a <see cref="System.String"/>, separated by the given separator.
+    /// </summary>
+    /// <returns>
+    /// A string representation of all of the items within the <paramref name="collection"/>.
+    /// </returns>
+    /// <param name='collection'>
+    /// The collection for which to generate the string representation.
+    /// </param>
+    /// <param name='separator'>
+    /// A separator sequence to appear between every item.
+    /// </param>
+    /// <exception cref='ArgumentNullException'>
+    /// Is thrown when an argument passed to a method is invalid because it is <see langword="null" /> .
+    /// </exception>
+    public static string CreateSeparatedString(IEnumerable collection, string separator)
+    {
+      StringBuilder output = new StringBuilder();
+      
+      if(collection == null)
+      {
+        throw new ArgumentNullException ("collection");
+      }
+      else if(separator == null)
+      {
+        throw new ArgumentNullException ("separator");
+      }
+      
+      foreach(object item in collection)
+      {
+        output.Append(item.ToString());
+        output.Append(separator);
+      }
+      
+      if(separator.Length > 0)
+      {
+        output.Remove(output.Length - separator.Length, separator.Length);
+      }
+      
+      return output.ToString();
     }
   }
 }
--- Test.CSF/Collections/TestIEnumerableExtensions.cs
+++ Test.CSF/Collections/TestIEnumerableExtensions.cs
@@ -1,21 +1,18 @@
 using System;
 using NUnit.Framework;
-using CSF;
+using CSF.Collections;
 
-namespace Test.CSF
+namespace Test.CSF.Collections
 {
   [TestFixture]
   public class TestIEnumerableExtensions
   {
     [Test]
-    [Description("This test may be removed in v3.x and upwards, as it tests obsolete functionality.")]
     public void TestToSeparatedString()
     {
       int[] foo = {1, 2, 3, 4, 5, 10, 20};
-#pragma warning disable 618
       Assert.AreEqual("1, 2, 3, 4, 5, 10, 20", foo.ToSeparatedString(", "), "Correct with ', ' separator");
       Assert.AreEqual("123451020", foo.ToSeparatedString(String.Empty), "Correct with empty separator");
-#pragma warning restore 618
     }
   }
 }
