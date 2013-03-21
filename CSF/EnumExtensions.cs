9e1997a8da581dedc9df7cad686f5ee11c538bfb
Craig Fowler <craig@craigfowler.me.uk>
2013-03-21 00:37:59 +0000

    Resolve #16 - Add 'HasFlag' enumeration extension method.
    This analyses an enumeration value and determines whether or not it contains
    a given flag value.
    
    * Tests added
    
    * Two sister methods of AddFlag and RemoveFlag were dropped from this issue, as
      they are too complex to implement properly.  If they are desired then they can
      be added into a new issue of their own.

--- CSF/EnumExtensions.cs
+++ CSF/EnumExtensions.cs
@@ -19,6 +19,8 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
+using CSF.Reflection;
+using System.Reflection;
 
 namespace CSF
 {
@@ -46,6 +48,63 @@ namespace CSF
     public static bool IsDefinedValue(this Enum value)
     {
       return IsDefined(value);
+    }
+
+    /// <summary>
+    /// Determines whether the enumeration <paramref name="value"/> contains a given <paramref name="flagValue"/>.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This method is only useable on enumerations decorated with <c>System.FlagsAttribute</c>.
+    /// </para>
+    /// </remarks>
+    /// <returns>
+    /// <c>true</c> if the <paramref name="value"/> contains the <paramref name="flagValue"/>; otherwise, <c>false</c>.
+    /// </returns>
+    /// <param name='value'>
+    /// The enumeration value to analyse.
+    /// </param>
+    /// <param name='flagValue'>
+    /// An enumeration value to search for within the value.
+    /// </param>
+    public static bool HasFlag(this Enum value, Enum flagValue)
+    {
+      if(value == null)
+      {
+        throw new ArgumentNullException("value");
+      }
+      else if(flagValue == null)
+      {
+        throw new ArgumentNullException("flagValue");
+      }
+
+      Type enumType = value.GetType();
+      Type flagValueType = flagValue.GetType();
+
+      if(!enumType.HasAttribute<FlagsAttribute>())
+      {
+        throw new ArgumentException("The given enumeration type must be decorated with FlagsAttribute.", "value");
+      }
+      else if(enumType != flagValueType)
+      {
+        throw new ArgumentException("The flag-value to check for must be of the same enumeration type as the " +
+                                    "enumeration value to analyse.",
+                                    "flagValue");
+      }
+
+      Type underlyingType = Enum.GetUnderlyingType(enumType);
+
+      if(underlyingType != typeof(int))
+      {
+        throw new ArgumentException("The underlying type of flags-based enumerations to compare must be System.Int32," +
+                                    "other types are currently unsupported.",
+                                    "value");
+      }
+
+      int sourceEnum = (int) Convert.ChangeType(value, underlyingType);
+      int valueToCheck = (int) Convert.ChangeType(flagValue, underlyingType);
+
+      return (sourceEnum & valueToCheck) == valueToCheck;
     }
 
     #endregion
--- Test.CSF/TestEnumExtensions.cs
+++ Test.CSF/TestEnumExtensions.cs
@@ -21,6 +21,28 @@ namespace Test.CSF
       Assert.IsFalse(((SampleEnum) 8).IsDefinedValue());
     }
 
+    [Test]
+    public void TestHasFlagTrue()
+    {
+      FlagsEnum val = (FlagsEnum.One | FlagsEnum.Eight);
+      Assert.IsTrue(val.HasFlag(FlagsEnum.Eight));
+    }
+
+    [Test]
+    public void TestHasFlagFalse()
+    {
+      FlagsEnum val = (FlagsEnum.One | FlagsEnum.Eight);
+      Assert.IsFalse(val.HasFlag(FlagsEnum.Four));
+    }
+
+    [Test]
+    [ExpectedException(typeof(ArgumentException))]
+    public void TestHasFlagNotFlags()
+    {
+      SampleEnum val = SampleEnum.One;
+      val.HasFlag(SampleEnum.One);
+    }
+
     #endregion
 
     #region test enumeration
@@ -34,6 +56,18 @@ namespace Test.CSF
       Two   = 2,
       
       Three = 3
+    }
+
+    [Flags]
+    enum FlagsEnum : int
+    {
+      One = 1,
+
+      Two = 2,
+
+      Four = 4,
+
+      Eight = 8
     }
     
     #endregion
