1fa7dff528964f6d81076eedc10bffbd6af27fd5
Craig Fowler <craig@craigfowler.me.uk>
2013-04-11 00:15:39 +0100

    Resolve #37 - Implement 'AreContentsSameAs' extension method for IEnumerable<T>
    This method checks that two collections contain the same items, ignoring order.

--- Test.CSF/Collections/TestIEnumerableExtensions.cs
+++ Test.CSF/Collections/TestIEnumerableExtensions.cs
@@ -8,6 +8,8 @@ namespace Test.CSF.Collections
   [TestFixture]
   public class TestIEnumerableExtensions
   {
+    #region testing ToSeparatedString
+
     [Test]
     public void TestToSeparatedString()
     {
@@ -32,6 +34,116 @@ namespace Test.CSF.Collections
                       foo.ToSeparatedString(",", x => x.Year),
                       "Correct with ',' separator");
     }
+
+    #endregion
+
+    #region testing AreContentsSameAs
+
+    [Test]
+    public void TestAreContentsSameAs()
+    {
+      var collection1 = new string[] {
+        "foo", "foo", "bar", "baz"
+      };
+
+      var collection2 = new string[] {
+        "baz", "bar", "foo", "foo"
+      };
+
+      Assert.IsTrue(collection1.AreContentsSameAs(collection2), "Contents are the same");
+    }
+
+    [Test]
+    public void TestAreContentsSameAsRefEqual()
+    {
+      var collection1 = new string[] {
+        "foo", "foo", "bar", "baz"
+      };
+
+      Assert.IsTrue(collection1.AreContentsSameAs(collection1), "Contents are the same");
+    }
+
+    [Test]
+    public void TestAreContentsSameAsDifferentCounts()
+    {
+      var collection1 = new string[] {
+        "foo", "foo", "bar", "baz"
+      };
+
+      var collection2 = new string[] {
+        "baz", "bar", "foo", "bar"
+      };
+
+      Assert.IsFalse(collection1.AreContentsSameAs(collection2), "Contents are the same");
+    }
+
+    [Test]
+    public void TestAreContentsSameAsDifferentItems()
+    {
+      var collection1 = new string[] {
+        "foo", "foo", "bar", "wibble"
+      };
+
+      var collection2 = new string[] {
+        "baz", "bar", "foo", "foo"
+      };
+
+      Assert.IsFalse(collection1.AreContentsSameAs(collection2), "Contents are the same");
+    }
+
+    [Test]
+    public void TestAreContentsSameAsCustomComparer()
+    {
+      var collection1 = new string[] {
+        "foo", "foo", "flam", "flom"
+      };
+
+      var collection2 = new string[] {
+        "floop", "fwoop", "foo", "foo"
+      };
+
+      Assert.IsTrue(collection1.AreContentsSameAs(collection2, new FirstCharacterEqualityComparer()),
+                    "Contents are the same");
+    }
+
+    [Test]
+    public void TestAreContentsSameAsWithoutCustomComparer()
+    {
+      var collection1 = new string[] {
+        "foo", "foo", "flam", "flom"
+      };
+
+      var collection2 = new string[] {
+        "floop", "fwoop", "foo", "foo"
+      };
+
+      Assert.IsFalse(collection1.AreContentsSameAs(collection2), "Contents are the same");
+    }
+
+    #endregion
+
+    #region contained type
+
+    class FirstCharacterEqualityComparer : IEqualityComparer<string>
+    {
+      #region IEqualityComparer implementation
+
+      public bool Equals (string x, string y)
+      {
+        return (!String.IsNullOrEmpty(x)
+                && !String.IsNullOrEmpty(y)
+                && x[0] == y[0]);
+      }
+
+      public int GetHashCode (string obj)
+      {
+        return (obj.Length > 0)? obj[0].GetHashCode() : 0;
+      }
+
+      #endregion
+    }
+
+    #endregion
   }
 }
 
