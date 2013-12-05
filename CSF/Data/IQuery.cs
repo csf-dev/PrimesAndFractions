b564333d0dbb52b1f7248b3a80a38f5f622a751d
Craig Fowler <craig@craigfowler.me.uk>
2013-12-05 14:30:50 +0000

    Resolve #74 - Add IQuery<T> interface for components that provide queries
    This is a simple abstraction to a data-source.  It does not provide any
    functionality but it can solve some issues where access to the data-source
    is provided via an extension method (thus making it difficult to mock).

---
+++ CSF/Data/IQuery.cs
@@ -0,0 +1,37 @@
+//
+//  IQuery.cs
+//
+//  Author:
+//       Craig Fowler <craig@craigfowler.me.uk>
+//
+//  Copyright (c) 2013 Craig Fowler
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Linq;
+
+namespace CSF.Data
+{
+  /// <summary>
+  /// Interface for a generic query component that is capable of returning a queryable data source.
+  /// </summary>
+  public interface IQuery<TQueried> where TQueried : class
+  {
+    /// <summary>
+    /// Gets a new queryable data-source.
+    /// </summary>
+    IQueryable<TQueried> Query();
+  }
+}
+
