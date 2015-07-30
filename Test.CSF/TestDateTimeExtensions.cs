b5884af14f79bac2a2c3377a1803003f79f88b37
Craig Fowler <craig@csf-dev.com>
2015-07-30 21:45:37 +0100

    Trivial refactoring of an existing unit test to use TestCase

--- Test.CSF/TestDateTimeExtensions.cs
+++ Test.CSF/TestDateTimeExtensions.cs
@@ -7,31 +7,31 @@ namespace Test.CSF
   [TestFixture]
   public class TestDateTimeExtensions
   {
-    [Test]
-    public void TestAsAgeInYears()
+    #region tests
+
+    [TestCase(1976, 09, 18, 2012, 10, 03, 36)]
+    [TestCase(1976, 09, 18, 2012, 10, 20, 36)]
+    [TestCase(1976, 09, 18, 2012, 09, 03, 35)]
+    [TestCase(1976, 09, 18, 2012, 09, 20, 36)]
+    [TestCase(1976, 09, 18, 2012, 08, 03, 35)]
+    [TestCase(1976, 09, 18, 2012, 08, 20, 35)]
+    public void TestAsAgeInYears(int birthYear, int birthMonth, int birthDay,
+                                 int referenceYear, int referenceMonth, int referenceDay,
+                                 int expectedAge)
     {
-      DateTime birth, reference;
+      DateTime
+        birth = new DateTime(birthYear, birthMonth, birthDay),
+        reference = new DateTime(referenceYear, referenceMonth, referenceDay);
 
-      birth = new DateTime(1976, 09, 18);
-      reference = new DateTime(2012, 10, 03);
-
-      Assert.AreEqual(36, birth.AsAgeInYears(reference), reference.ToShortDateString());
-
-      reference = new DateTime(2012, 10, 20);
-      Assert.AreEqual(36, birth.AsAgeInYears(reference), reference.ToShortDateString());
-
-      reference = new DateTime(2012, 09, 03);
-      Assert.AreEqual(35, birth.AsAgeInYears(reference), reference.ToShortDateString());
-
-      reference = new DateTime(2012, 09, 20);
-      Assert.AreEqual(36, birth.AsAgeInYears(reference), reference.ToShortDateString());
-
-      reference = new DateTime(2012, 08, 03);
-      Assert.AreEqual(35, birth.AsAgeInYears(reference), reference.ToShortDateString());
-
-      reference = new DateTime(2012, 08, 20);
-      Assert.AreEqual(35, birth.AsAgeInYears(reference), reference.ToShortDateString());
+      Assert.AreEqual(expectedAge,
+                      birth.AsAgeInYears(reference),
+                      "Birthday {0}: Age {1} on {2}",
+                      birth.ToShortDateString(),
+                      expectedAge,
+                      reference.ToShortDateString());
     }
+
+    #endregion
   }
 }
 
