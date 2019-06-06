c4e6827034d9eb4d3145a4fe9213d64f9c53dc5a
Craig Fowler <craig.fowler@tracesolutions.co.uk>
2019-06-06 13:22:47 +0100

    WIP #112 - First stab at classes

---
+++ CSF/Collections/ListEqualityComparer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSF.Collections
+{
+    public class ListEqualityComparer<TItem> : IEqualityComparer, IEqualityComparer<IEnumerable<TItem>>
+    {
+        readonly IEqualityComparer<TItem> itemComparer;
+
+        bool IEqualityComparer.Equals(object x, object y)
+        {
+            return Equals(x as IEnumerable<TItem>, y as IEnumerable<TItem>);
+        }
+
+        int IEqualityComparer.GetHashCode(object obj)
+        {
+            return GetHashCode(obj as IEnumerable<TItem>);
+        }
+
+        public bool Equals(IEnumerable<TItem> x, IEnumerable<TItem> y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+
+            return x.SequenceEqual(y, itemComparer);
+        }
+
+        public int GetHashCode(IEnumerable<TItem> obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ListEqualityComparer() : this(null) {}
+
+        public ListEqualityComparer(IEqualityComparer<TItem> itemComparer)
+        {
+            this.itemComparer = itemComparer;
+        }
+    }
+}
---
+++ CSF/Collections/SetEqualityComparer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CSF.Collections
+{
+    public class SetEqualityComparer<TItem> : IEqualityComparer, IEqualityComparer<IEnumerable<TItem>>
+    {
+        bool IEqualityComparer.Equals(object x, object y)
+        {
+            return Equals(x as IEnumerable<TItem>, y as IEnumerable<TItem>);
+        }
+
+        int IEqualityComparer.GetHashCode(object obj)
+        {
+            return GetHashCode(obj as IEnumerable<TItem>);
+        }
+
+        public bool Equals(IEnumerable<TItem> x, IEnumerable<TItem> y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+
+            throw new System.NotImplementedException();
+        }
+
+        public int GetHashCode(IEnumerable<TItem> obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
