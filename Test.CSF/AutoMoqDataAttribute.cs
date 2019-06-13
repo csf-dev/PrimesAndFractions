5ce7620418d2122f7b84072e775de88bb4221980
Craig Fowler <craig.fowler@tracesolutions.co.uk>
2019-06-13 13:05:43 +0100

    WIP #112 - Add (empty) tests for new functionality
    These still need to be written

---
+++ Test.CSF/AutoMoqDataAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.NUnit3;
+
+namespace Test.CSF
+{
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute() : base(new Fixture().Customize(new AutoMoqCustomization())) {}
+    }
+}
---
+++ Test.CSF/Collections/SetEqualityComparerTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace Test.CSF.Collections
+{
+    [TestFixture,Parallelizable]
+    public class SetEqualityComparerTests
+    {
+        
+    }
+}
